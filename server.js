503b0b33f89b15b93b5b4d3d7eb9136255dca14b
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-11-06 15:45:16 -0700

    ACP and merge before deployment

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const app = require('./lib/app.js');
 const pool = require('./lib/utils/pool.js');
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 7892;
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
